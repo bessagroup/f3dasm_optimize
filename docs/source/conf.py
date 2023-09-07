66a584039fe33027e1ff53ec81e3fcfecd40f359
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2023-09-07 15:57:38 -0400

    added f3dasm intersphinx mapping

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,6 +94,7 @@ intersphinx_mapping = {
     "scipy": ("https://docs.scipy.org/doc/scipy/reference/", None),
     "matplotlib": ("https://matplotlib.org/stable/", None),
     "pandas": ('https://pandas.pydata.org/docs/', None),
+    "f3dasm": ('https://bessagroup.github.io/f3dasm/', None),
 }
 
 # -- Options for HTML output -------------------------------------------------
