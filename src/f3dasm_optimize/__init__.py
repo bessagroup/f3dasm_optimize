1e96049bdd02a482e12b38c7cdf60b77f19cd85e
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2025-01-14 09:12:13 +0100

    removed tensorflow and pygmo optimizers

--- src/f3dasm_optimize/__init__.py
+++ src/f3dasm_optimize/__init__.py
@@ -2,6 +2,7 @@
 # =============================================================================
 
 # Local
+from .__version__ import __version__
 from ._src import optimizers_extension
 from ._src._imports import try_import
 
@@ -11,16 +12,9 @@ with try_import() as _evosax_imports:
 with try_import() as _nevergrad_imports:
     from ._src.nevergrad_optimizers import de_nevergrad, pso_nevergrad
 
-with try_import() as _pygmo_imports:
-    from ._src.pygmo_optimizers import (cmaes_pygmo, de_pygmo, pso_pygmo, sade,
-                                        sea, sga, simanneal_pygmo, xnes)
-
 with try_import() as _optuna_imports:
     from ._src.optuna_optimizers import tpe_sampler
 
-with try_import() as _tensorflow_imports:
-    from ._src.tensorflow_optimizers import (adam_tensorflow, adamax, ftrl,
-                                             nadam, rmsprop, sgd_tensorflow)
 
 with try_import() as _optax_imports:
     from ._src.optax_optimizers import adam, sgd
@@ -37,28 +31,13 @@ __status__ = 'Stable'
 __all__ = [
     'optimizers_extension',
     'adam',
-    'adam_tensorflow',
-    'adamax',
     'cmaes',
-    'cmaes_pygmo',
     'de',
     'de_nevergrad',
-    'de_pygmo',
-    'ftrl',
-    'nadam',
     'pso',
     'pso_nevergrad',
-    'pso_pygmo',
-    'rmsprop',
-    'sade',
-    'sea',
-    'sga',
-    'sgd_tensorflow',
     'sgd',
     'simanneal',
-    'simanneal_pygmo',
     'tpe_sampler',
-    'xnes',
+    '__version__',
 ]
-
-__version__ = '1.5.5'
--- src/f3dasm_optimize/_src/__init__.py
+++ src/f3dasm_optimize/_src/__init__.py
@@ -11,16 +11,8 @@ with try_import() as _evosax_imports:
 with try_import() as _nevergrad_imports:
     from .nevergrad_optimizers import de_nevergrad, pso_nevergrad
 
-# with try_import() as _pygmo_imports:
-#     from .pygmo_optimizers import (cmaes_pygmo, de_pygmo, pso_pygmo, sade, sea,
-#                                    sga, simanneal_pygmo, xnes)
-
 with try_import() as _optuna_imports:
     from .optuna_optimizers import tpe_sampler
-
-# with try_import() as _tensorflow_imports:
-#     from .tensorflow_optimizers import (adam_tensorflow, adamax, ftrl, nadam,
-#                                         rmsprop, sgd_tensorflow)
 
 with try_import() as _optax_imports:
     from .optax_optimizers import adam, sgd
@@ -38,16 +30,8 @@ __status__ = 'Stable'
 def optimizers_extension():
     optimizer_list = []
 
-    # if _pygmo_imports.is_successful():
-    #     optimizer_list.extend([cmaes_pygmo, de_pygmo, pso_pygmo, sade, sea,
-    #                            sga, simanneal_pygmo, xnes])
-
     if _optuna_imports.is_successful():
         optimizer_list.extend([tpe_sampler])
-
-    # if _tensorflow_imports.is_successful():
-    #     optimizer_list.extend([adam_tensorflow, adamax, ftrl, nadam,
-    #                            rmsprop, sgd_tensorflow])
 
     if _evosax_imports.is_successful():
         optimizer_list.extend([cmaes, de, pso, simanneal])
@@ -63,26 +47,12 @@ def optimizers_extension():
 
 __all__ = [
     'adam',
-    # 'adam_tensorflow',
-    # 'adamax',
     'cmaes',
-    # 'cmaes_pygmo',
     'de',
     'de_nevergrad',
-    # 'de_pygmo',
-    # 'ftrl',
-    # 'nadam',
     'pso',
     'pso_nevergrad',
-    # 'pso_pygmo',
-    # 'rmsprop',
-    # 'sade',
-    # 'sea',
-    # 'sga',
     'sgd',
-    # 'sgd_tensorflow',
     'simanneal',
-    # 'simanneal_pygmo',
     'tpe_sampler',
-    # 'xnes',
 ]
